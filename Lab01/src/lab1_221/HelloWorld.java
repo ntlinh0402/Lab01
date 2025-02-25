5bc9d15a4fb2b0515f976dd68c23ef983fddfc43
Linh Nguyen <nguyenthuylinh040205@gmail.com>
2025-02-25 16:33:58 +0700

    Again Complete assignment 2.2.1

---
+++ Lab01/src/lab1_221/HelloWorld.java
@@ -0,0 +1,13 @@
+package lab1_221;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Xin chao \n cac ban!");
+		System.out.print("Hello \t world!");
+
+	}
+
+}
+
